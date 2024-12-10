9164e646f7b7b0fbe1b7d9392176a7df5e0ac2b3
Christian Schüller <cschueller@nvidia.com>
2024-12-10 19:57:56 +0100

    Work on action test

--- tests/test_configs/with_custom_action_v2_x/actions.py
+++ tests/test_configs/with_custom_action_v2_x/actions.py
@@ -13,9 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import asyncio
+from typing import Optional
+
 from nemoguardrails.actions import action
+from nemoguardrails.actions.action_dispatcher import ActionEventGenerator
 
 
-@action(name="CustomTestAction")
-async def custom_test(context: dict, param: int):
+@action(name="CustomTestAction", is_system_action=True, execute_async=True)
+async def custom_test(context: dict, param: int, event_generator: ActionEventGenerator):
+    for i in range(1, 5):
+        await asyncio.sleep(5)
+        await event_generator.send_action_update_event("Test", {f"value {i}": 10})
     return param + context["value"]
+
+
+@action(name="CustomActionWithUpdateEventsAction")
+async def custom_action_with_update_events(context: Optional[dict] = None, **kwargs):
+    return True
